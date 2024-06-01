1329811bc677fb5eb11f072437bcf48e5fc3c2cf
ImLuu <45904735+ImLuu@users.noreply.github.com>
2024-06-01 13:18:25 -0400

    creada las rutas y controladores

---
+++ Backend/controllers/index.controllers.js
@@ -0,0 +1,117 @@
+const database = require('../database')
+const db= database.getConnection()
+
+//controllers
+//get all the products
+exports.getProducts = async (req, res) => {
+    const connection = await db;
+    if (connection) {
+        try {
+            const answ = await connection.query("SELECT * FROM product");
+            if (answ) {
+               return res.json(answ);
+            } else {
+                return res.status(404).json({ msg: 'Products not found' });
+            }
+        } catch (err) {
+            return res.status(500).json({ msg: 'Internal Server Error' });
+        }
+    } else {
+        return res.status(403).json({ msg: 'Connection error' });
+    }
+};
+//get all the categories
+exports.getCategories = async (req, res) => {
+    const connection = await db;
+    if (connection) {
+        try {
+            const answ = await connection.query("select * from category ");
+            if (answ) {
+                return res.json(answ)
+            } else {
+                return res.status(404).json({ msg: 'Categories not found ' })
+            }
+
+        } catch (err) {
+            return res.status(500).json({ msg: 'Internal Server Error ' })
+        }
+    } else {
+        return res.status(403).json({ msg: 'Connection error ' })
+    }
+}
+
+//get a specific product , search by ID
+exports.getProductByID = async (req, res) => {
+    const connection = await db;
+    if (connection) {
+        try {
+            const { id } = req.params;
+            if (id != null) {
+                const answ = await connection.query("select * from product where id = ? ", [id]);
+                if (answ) {
+                    return res.json(answ)
+                } else {
+                    return res.status(404).json({ msg: 'Category not found' })
+                }
+            } else {
+                return res.json({ msg: "ID not found" })
+            }
+
+        } catch (err) {
+            return res.status(500).json({ msg: 'Internal Server Error ' })
+        }
+    } else {
+        return res.status(403).json({ msg: 'Connection error ' })
+    }
+}
+
+//get a specific category , search by ID
+exports.getCategoryByID = async (req, res) => {
+    const connection = await db;
+    if (connection) {
+        try {
+            const { id } = req.params;
+            if (id != null) {
+                const answ = await connection.query("select * from category where id = ? ", [id]);
+                if (answ) {
+                    return res.json(answ)
+                } else {
+                    return res.status(404).json({ msg: 'Product not found' })
+                }
+            } else {
+                return res.json({ msg: "ID not found" })
+            }
+
+        } catch (err) {
+            return res.status(500).json({ msg: 'Internal Server Error ' })
+        }
+    } else {
+        return res.status(403).json({ msg: 'Connection error ' })
+    }
+
+}
+
+//get the info of all the products with the same ID , search by ID
+exports.getSearchByID = async (req, res) => {
+    const connection = await db;
+    if (connection) {
+        try {
+            const { id } = req.params;
+            if (id != null) {
+                const answ = await connection.query('select tabla1.name_category , p.name as name_product , p.url_image , p.price , p.discount from product p inner join (select c.id as id_category, c.name as name_category from category c where id = ?) as tabla1 on tabla1.id_category = p.category', [id]);
+                if (answ) {
+                    return res.json(answ)
+                } else {
+                    return res.status(404).json({ msg: " Error getting information " })
+                }
+            } else {
+                return res.json({ msg: "ID not found " })
+            }
+
+        } catch (err) {
+            return res.status(500).json({ msg: 'Internal Server Error ' })
+        }
+    } else {
+        return res.status(403).json({ msg: 'connection error ' })
+    }
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const database = require('./database')
 const morgan = require("morgan")
+const routes = require("./routes/index.routes")
 
 //Config
 const app = express();
@@ -13,126 +13,6 @@ console.log("PORT: " + app.get("port"));
 
 app.use(morgan("dev"))
 
-db = database.getConnection();
-if (db) {
-    console.log("DB connected");
-}else{
-    console.log("DB connection ERROR");
-}
-//API
-// search all the products
-app.get("/products", async (req, res) => {
-    const connection = await db;
-    if (connection) {
-        try {
-            const answ = await connection.query("select * from product ");
-            if (answ) {
-                res.json(answ)
-            } else {
-                res.status(404).json({ msg: 'Products not found ' })
-            }
-
-        } catch (err) {
-            res.status(500).json({ msg: 'Internal Server Error ' })
-        }
-    } else {
-        res.status(403).json({ msg: 'Connection error ' })
-    }
-})
-// search all the categories
-app.get("/categories", async (req, res) => {
-    const connection = await db;
-    if (connection) {
-        try {
-            const answ = await connection.query("select * from category ");
-            if (answ) {
-                res.json(answ)
-            } else {
-                res.status(404).json({ msg: 'Categories not found ' })
-            }
-
-        } catch (err) {
-            res.status(500).json({ msg: 'Internal Server Error ' })
-        }
-    } else {
-        res.status(403).json({ msg: 'Connection error ' })
-    }
-})
-// search for a specific product
-app.get("/product/:id", async (req, res) => {
-    const connection = await db;
-    if (connection) {
-        try {
-            const { id } = req.params;
-            if (id != null) {
-                const answ = await connection.query("select * from product where id = ? ", [id]);
-                if (answ) {
-                    res.json(answ)
-                } else {
-                    res.status(404).json({ msg: 'Product not found' })
-                }
-            } else {
-                res.json({ msg: "ID not found" })
-            }
-
-        } catch (err) {
-            res.status(500).json({ msg: 'Internal Server Error ' })
-        }
-    } else {
-        res.status(403).json({ msg: 'Connection error ' })
-    }
-
-})
-
-// search for a specific category
-app.get("/category/:id", async (req, res) => {
-    const connection = await db;
-    if (connection) {
-        try {
-            const { id } = req.params;
-            if (id != null) {
-                const answ = await connection.query("select id ,name from category where id = ? ", [id]);
-                if (answ) {
-                    res.json(answ)
-                } else {
-                    res.status(404).json({ msg: 'Category not found' })
-                }
-            } else {
-                res.json({ msg: "ID not found" })
-            }
-
-        } catch (err) {
-            res.status(500).json({ msg: 'Internal Server Error ' })
-        }
-    } else {
-        res.status(403).json({ msg: 'Connection error ' })
-    }
-})
-
-//Codigo para la prueba
-
-//Search by ID all the products with the same ID
-app.get("/search/:id", async (req, res) => {
-    const connection = await db;
-    if (connection) {
-        try {
-            const { id } = req.params;
-            if (id != null) {
-                const answ = await connection.query('select tabla1.name_category , p.name as name_product , p.url_image , p.price , p.discount from product p inner join (select c.id as id_category, c.name as name_category from category c where id = ?) as tabla1 on tabla1.id_category = p.category', [id]);
-                if (answ) {
-                    res.json(answ)
-                } else {
-                    res.status(404).json({ msg: " Error getting information " })
-                }
-            } else {
-                res.json({ msg: "ID not found " })
-            }
-
-        } catch (err) {
-            res.status(500).json({ msg: 'Internal Server Error ' })
-        }
-    } else {
-        res.status(403).json({ msg: 'connection error ' })
-    }
-})
+//Using the routes
+app.use(routes);
 
