a0401a693dd4fcdf6dab45f2b395888b90de1c16
ImLuu <45904735+ImLuu@users.noreply.github.com>
2024-05-31 20:13:32 -0400

    Creacion primer parte pagina web

--- Backend/database.js
+++ Backend/database.js
@@ -2,12 +2,12 @@ const mysql = require('promise-mysql');
 require('dotenv').config();
 
 //Creamos la conexión MySQL 
-const conexion = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'itnovai-test.czny55quvxhk.us-east-1.rds.amazonaws.com',
   user: 'itnovai_test',
   database: 'itnovai_test',
   password:'itnovai_test'
 });
 
-const getConexion = async () => conexion;
-module.exports = {getConexion}
+const getConnection = async () => connection;
+module.exports = {getConnection}
--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const database = require('./database')
 const morgan = require("morgan")
-//Configuracion
+
+
+//Config
 const app = express();
 
 //Asignamos un puerto X
@@ -10,121 +12,95 @@ app.use(express.json());
 
 //Comprobamos el puerto
 app.listen(app.get('port'));
-console.log("Conexión al puerto: " + app.get("port"));
+console.log("PORT: " + app.get("port"));
 
 app.use(morgan("dev"))
 
 //Apis de prueba 
-
 app.get("/products", async (req, res) => {
-    const conexion = await database.getConexion();
-    if (conexion) {
+    const connection = await database.getConnection();
+    if (connection) {
         try {
-            const resultados = await conexion.query("select * from product ");
-            if (resultados) {
-                res.json(resultados)
+            const answ = await connection.query("select * from product ");
+            if (answ) {
+                res.json(answ)
             } else {
-                res.status(404).json({ msg: 'Error al obtener productos' })
+                res.status(404).json({ msg: 'Products not found ' })
             }
 
         } catch (err) {
-            res.status(500).json({ msg: 'Error' })
+            res.status(500).json({ msg: 'Internal Server Error ' })
         }
     } else {
-        res.status(403).json({ msg: 'Error de conexión a BDD' })
+        res.status(403).json({ msg: 'Connection error ' })
     }
 })
 
 app.get("/categories", async (req, res) => {
-    const conexion = await database.getConexion();
-    if (conexion) {
+    const connection = await database.getConnection();
+    if (connection) {
         try {
-            const resultados = await conexion.query("select * from category ");
-            if (resultados) {
-                res.json(resultados)
+            const answ = await connection.query("select * from category ");
+            if (answ) {
+                res.json(answ)
             } else {
-                res.status(404).json({ msg: 'Error al obtener categorias' })
+                res.status(404).json({ msg: 'Categories not found ' })
             }
 
         } catch (err) {
-            res.status(500).json({ msg: 'Error' })
+            res.status(500).json({ msg: 'Internal Server Error ' })
         }
     } else {
-        res.status(403).json({ msg: 'Error de conexión a BDD' })
+        res.status(403).json({ msg: 'Connection error ' })
     }
 })
 
 app.get("/product/:id", async (req, res) => {
-    const conexion = await database.getConexion();
-    if (conexion) {
+    const connection = await database.getConnection();
+    if (connection) {
         try {
             const { id } = req.params;
             if (id != null) {
-                const resultados = await conexion.query("select * from product where id = ? ", [id]);
-                if (resultados) {
-                    res.json(resultados)
+                const answ = await connection.query("select * from product where id = ? ", [id]);
+                if (answ) {
+                    res.json(answ)
                 } else {
-                    res.status(404).json({ msg: 'Error al obtener producto' })
+                    res.status(404).json({ msg: 'Product not found' })
                 }
             } else {
-                res.json({ msg: "ID no encontrado" })
+                res.json({ msg: "ID not found" })
             }
 
         } catch (err) {
-            res.status(500).json({ msg: 'Error' })
+            res.status(500).json({ msg: 'Internal Server Error ' })
         }
     } else {
-        res.status(403).json({ msg: 'Error de conexión a BDD' })
-    }
-
-})
-
-app.get("/product/:category", async (req, res) => {
-    const conexion = await database.getConexion();
-    if (conexion) {
-        try {
-            const { category } = req.params;
-            if (id != null) {
-                const resultados = await conexion.query("select * from product where category = ? ", [category]);
-                if (resultados) {
-                    res.json(resultados)
-                } else {
-                    res.status(404).json({ msg: 'Error al obtener producto' })
-                }
-            } else {
-                res.json({ msg: "ID no encontrado" })
-            }
-
-        } catch (err) {
-            res.status(500).json({ msg: 'Error' })
-        }
-    } else {
-        res.status(403).json({ msg: 'Error de conexión a BDD' })
+        res.status(403).json({ msg: 'Connection error ' })
     }
 
 })
 
 app.get("/category/:id", async (req, res) => {
-    const conexion = await database.getConexion();
-    if (conexion) {
+    const connection = await database.getConnection();
+    if (connection) {
         try {
             const { id } = req.params;
             if (id != null) {
-                const resultados = await conexion.query("select id ,name from category where id = ? ", [id]);
-                if (resultados) {
-                    res.json(resultados)
+                const answ = await connection.query("select id ,name from category where id = ? ", [id]);
+                if (answ) {
+                    res.json(answ)
                 } else {
-                    res.status(404).json({ msg: 'Error al obtener categoria' })
+                    res.status(404).json({ msg: 'Category not found' })
                 }
             } else {
-                res.json({ msg: "ID no encontrado" })
+                res.json({ msg: "ID not found" })
             }
 
         } catch (err) {
-            res.status(500).json({ msg: 'Error' })
+            res.status(500).json({ msg: 'Internal Server Error ' })
         }
     } else {
-        res.status(403).json({ error: 'Error de conexión a BDD' })
+        res.status(403).json({ msg: 'Connection error ' })
     }
 })
 
@@ -132,26 +108,26 @@ app.get("/category/:id", async (req, res) => {
 
 //Busqueda de los productos por categoria 
 app.get("/search/:id", async (req, res) => {
-    const conexion = await database.getConexion();
-    if (conexion) {
+    const connection = await database.getConnection();
+    if (connection) {
         try {
             const { id } = req.params;
             if (id != null) {
-                const resultados = await conexion.query('select tabla1.name_category , p.name as name_product , p.url_image , p.price , p.discount from product p inner join (select c.id as id_category, c.name as name_category from category c where id = ?) as tabla1 on tabla1.id_category = p.category', [id]);
-                if (resultados) {
-                    res.json(resultados)
+                const answ = await connection.query('select tabla1.name_category , p.name as name_product , p.url_image , p.price , p.discount from product p inner join (select c.id as id_category, c.name as name_category from category c where id = ?) as tabla1 on tabla1.id_category = p.category', [id]);
+                if (answ) {
+                    res.json(answ)
                 } else {
-                    res.status(404).json({ msg: "Error al obtener información" })
+                    res.status(404).json({ msg: " Error getting information " })
                 }
             } else {
-                res.json({ msg: "ID no encontrado" })
+                res.json({ msg: "ID not found " })
             }
 
         } catch (err) {
-            res.status(500).json({ msg: 'Error' })
+            res.status(500).json({ msg: 'Internal Server Error ' })
         }
     } else {
-        res.status(403).json({ msg: 'Error de conexión a BDD' })
+        res.status(403).json({ msg: 'connection error ' })
     }
 })
 
