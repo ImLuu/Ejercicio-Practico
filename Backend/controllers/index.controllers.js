74edbe812b0851868ef683e15db1e77827738ea3
ImLuu <45904735+ImLuu@users.noreply.github.com>
2024-06-01 15:58:51 -0400

    probando cosas en el front

--- Backend/controllers/index.controllers.js
+++ Backend/controllers/index.controllers.js
@@ -25,7 +25,7 @@ exports.getCategories = async (req, res) => {
     const connection = await db;
     if (connection) {
         try {
-            const answ = await connection.query("select * from category ");
+            const answ = await connection.query("select name from category ");
             if (answ) {
                 return res.json(answ)
             } else {
@@ -97,7 +97,7 @@ exports.getSearchByID = async (req, res) => {
     if (connection) {
         try {
             const { id } = req.params;
-            if (id != null) {
+            if (id!= null) {
                 const answ = await connection.query('select tabla1.name_category , p.name as name_product , p.url_image , p.price , p.discount from product p inner join (select c.id as id_category, c.name as name_category from category c where id = ?) as tabla1 on tabla1.id_category = p.category', [id]);
                 if (answ) {
                     return res.json(answ)
--- Backend/routes/index.routes.js
+++ Backend/routes/index.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/index.controllers");
 
-//Routes
+//Routes to controller
 router.get('/products', controller.getProducts);
 router.get("/categories",controller.getCategories)
 router.get("/category/:id",controller.getCategoryByID)
 router.get("/product/:id",controller.getProductByID)
 router.get("/search/:id",controller.getSearchByID)
+
 module.exports = router;
