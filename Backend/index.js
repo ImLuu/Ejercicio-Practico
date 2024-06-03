79f334e09eaa315394b2bdb95d69ab08e0d84a18
ImLuu <45904735+ImLuu@users.noreply.github.com>
2024-06-03 19:04:27 -0400

    cambio de puerto

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const morgan = require("morgan");
 const routes = require("./routes/index.routes");
 const cors = require("cors");
+const PORT = require("dotenv").config();
 
 //Config
 const app = express();
 
 //PORT
-app.set('port', 4000)
+app.set('port', process.env.PORT)
 app.use(express.json());
 app.use(cors());
 app.listen(app.get('port'));
