08f1ac80ce69e5132b135b0e177eab0f0167255c
ImLuu <45904735+ImLuu@users.noreply.github.com>
2024-06-04 09:27:12 -0400

    Update routes.js

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require("../controllers/index.controllers");
+const controller = require("../controllers/controllers");
 
 //Routes to controller
 router.get("/",controller.getGreetings )
